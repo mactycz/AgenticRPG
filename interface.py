3a2f3f41f158638362900d4e9ace540cf884f450
mactycz <mactycz@gmail.com>
2024-10-30 12:43:44 +0100

    fixed the git messages

--- interface.py
+++ interface.py
@@ -19,11 +19,7 @@ with gr.Blocks(fill_width=True) as demo:
     with gr.Row(visible=False) as main_interface:
         with gr.Column():
             with gr.Row():
-<<<<<<< HEAD
-                UserStory = gr.Textbox(label="Story explaination",scale=3,)
-=======
                 user_story = gr.Textbox(label="Story explaination",scale=3,)
->>>>>>> master
                 generate_story_button = gr.Button("Generate story")
             with gr.Row():
                 story = gr.Textbox(label="Story",scale=3)
@@ -31,26 +27,16 @@ with gr.Blocks(fill_width=True) as demo:
                 image_prompt = gr.Textbox(label="Image prompt",scale=3)
                 summary = gr.Textbox(label="Summary",scale=3)
             with gr.Row():
-<<<<<<< HEAD
-                UserStory = gr.Textbox(label="User input",scale=3,)
-=======
                 user_continuation = gr.Textbox(label="User input",scale=3,)
->>>>>>> master
                 generate_continuation = gr.Button("Generate continuation")
         with gr.Column():
             change_api = gr.Button("Change API")
             image= gr.Image(label="Image",height=600,)
             image_button = gr.Button("Generate Image")
 
-<<<<<<< HEAD
-    generate_story_button.click(fn=hf.GenerateStory,inputs=UserStory,outputs=[story,characters,image_prompt,summary], api_name="generateStory")
-    image_button.click(fn=hf.GenerateImage,inputs=image_prompt,outputs=image,api_name="generateImage")
-    generate_continuation.click(fn=hf.GenerateContinuation,inputs =[summary,UserStory],outputs=[story,image_prompt,summary], api_name="generateContinuation")
-=======
     generate_story_button.click(fn=hf.GenerateStory,inputs=user_story,outputs=[story,characters,image_prompt,summary], api_name="generateStory")
     image_button.click(fn=hf.GenerateImage,inputs=image_prompt,outputs=image,api_name="generateImage")
     generate_continuation.click(fn=hf.GenerateContinuation,inputs =[summary,user_continuation],outputs=[story,image_prompt,summary], api_name="generateContinuation")
->>>>>>> master
     api_key_button.click(fn=add_key_and_show_interface,inputs=[api_selection,api_key_input],outputs=[main_interface,selection_interface])
     change_api.click(fn=add_key_and_show_interface,inputs=[api_selection,api_key_input],outputs=[selection_interface,main_interface])
 
