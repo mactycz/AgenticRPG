cbc071a6f500f1de5a2b006a9de7c400943dd3c0
mactycz <mactycz@gmail.com>
2024-10-30 14:59:52 +0100

    cleaned up interface so you can choose either api key or enviromental variable from dropdown

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
 from loadModel import loadModel
 from apis import hf,local,openAI,Anthropic
+import gradio as gr
+#API key interface - disappears after providing api and shows main interface
+def add_key_and_show_interface(api_choice, provided_api_key,provided_api_token):
+    global api_key, api_token
+    if provided_api_key:
+        api_key = provided_api_key
+    elif provided_api_token:
+        api_token = provided_api_token
+    else:
+        api_key = ""
+    if provided_api_key:
+        return gr.update(visible=True),gr.update(visible=False)
+    if provided_api_token:
+        return gr.update(visible=True),gr.update(visible=False)
+    return gr.update(visible=False),gr.update(visible=True)
 
-
+def update_auth_method(selected_auth):
+    if selected_auth == 'API key':
+        return gr.update(visible=True), gr.update(visible=False),
+    elif selected_auth == 'Enviromental variable token':
+        return gr.update(visible=False), gr.update(visible=True)
+    else:
+        return gr.update(visible=False), gr.update(visible=False)
 
--- interface.py
+++ interface.py
@@ -2,31 +2,20 @@ import gradio as gr
 from app import *
 
 dropdown_options = ['Local','Huggingface API','OpenAI','Anthropic']
+dropdown_options_api = ['','API key', 'Enviromental variable token']
 api_key = ""
 api_token=""
-#API key interface - disappears after providing api and shows main interface
-def add_key_and_show_interface(api_choice, provided_api_key,provided_api_token):
-    global api_key, api_token
-    if provided_api_key:
-        api_key = provided_api_key
-    elif provided_api_token:
-        api_token = provided_api_token
-    else:
-        api_key = ""
-    if provided_api_key:
-        return gr.update(visible=True),gr.update(visible=False)
-    if provided_api_token:
-        return gr.update(visible=True),gr.update(visible=False)
-    return gr.update(visible=False),gr.update(visible=True)
 
-with gr.Blocks(fill_width=True) as demo:
+
+with gr.Blocks(fill_width=True)as demo:
 
     with gr.Row(visible=True) as selection_interface:
         api_selection = gr.Dropdown(choices=dropdown_options, label="Select API", interactive=True)
-        api_key_input = gr.Textbox(label="Enter API Key", placeholder="API Key", type="password", interactive=True)
-        api_token = gr.Textbox(label="Enter Enviromental variable", placeholder="Token",interactive=True)
-        api_key_button = gr.Button("Add API Key",)
-
+        api_dropdown= gr.Dropdown(choices=dropdown_options_api, label="Select auth method", interactive=True)
+        api_key_input = gr.Textbox(label="Enter API Key", placeholder="API Key", type="password", interactive=True,visible=False)
+        api_token_input = gr.Textbox(label="Enter Enviromental variable", interactive=True, visible=False)
+        api_key_button = gr.Button("Add API Key")
+        api_dropdown.change(fn=update_auth_method, inputs=api_dropdown, outputs=[api_key_input, api_token_input])
     with gr.Row(visible=False) as main_interface:
         with gr.Column():
             with gr.Row():
@@ -48,7 +37,8 @@ with gr.Blocks(fill_width=True) as demo:
     generate_story_button.click(fn=hf.GenerateStory,inputs=user_story,outputs=[story,characters,image_prompt,summary], api_name="generateStory")
     image_button.click(fn=hf.GenerateImage,inputs=image_prompt,outputs=image,api_name="generateImage")
     generate_continuation.click(fn=hf.GenerateContinuation,inputs =[summary,user_continuation],outputs=[story,image_prompt,summary], api_name="generateContinuation")
-    api_key_button.click(fn=add_key_and_show_interface,inputs=[api_selection,api_key_input,api_token],outputs=[main_interface,selection_interface])
-    change_api.click(fn=add_key_and_show_interface,inputs=[api_selection,api_key_input,api_token],outputs=[selection_interface,main_interface])
+    api_key_button.click(fn=add_key_and_show_interface,inputs=[api_selection,api_key_input,api_token_input],outputs=[main_interface,selection_interface])
+    change_api.click(fn=add_key_and_show_interface,inputs=[api_selection,api_key_input,api_token_input],outputs=[selection_interface,main_interface])
+
 
 demo.launch()
