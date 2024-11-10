187a799b6bf6b7cc732cd51a786ddccee4ebccbe
mactycz <mactycz@gmail.com>
2024-11-10 20:37:22 +0100

    corrected prompts

--- prompts.py
+++ prompts.py
@@ -1,4 +1,4 @@
-localPromptStory = f"Yoo are a game master and a narrator for an rpg session. Generate text for the story up to 500 words according to theme and user input. Leave options for the player interaction."
-summarize_for_image= "Summarize this story into one prompt for image generation model. Focus the most important aspect that could be visualized - a place, a character or an event. Make it visual and detailed up to 100 words. It should enrich the story."
-abcd_options = "Generate 4 options for the player to choose from. Each option should be a possible action or a choice that the player can make. Make them different and interesting. Each option should be up to 50 words long."
-initialize_story="Welcome to the RPG adventure! Describe the theme of the story, and I'll narrate the journey."
+localPromptStory = f"Yoo are a game master and a narrator for an rpg session. Generate text for the story up to 500 words according to theme and user input. Treat it as a casual RPG seesion on friday night. Player should interact with the story."
+summarize_for_image= "Summarize this story into one prompt for image generation model. Focus the most important aspect that could be visualized - a place, a character or an event. Make it visual and detailed up to 100 words. It should enrich the story. "
+abcd_options = "Generate 4 options for the player to choose from. Each option should be a possible action or a choice that the player can make. Make them different and interesting. Each option should be up to 50 words long. "
+initialize_story="Welcome to the RPG adventure! Describe the theme of the story, and I'll be a game master. "
