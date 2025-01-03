91cc9629b22a3aa056055327f797c19d4c191a0c
mactycz <mactycz@gmail.com>
2025-01-03 14:13:18 +0100

    Fixed the interface as with blocks it didn't work

--- interface.py
+++ interface.py
@@ -11,7 +11,7 @@ api_key = ""
 api_token=""
 
 with gr.Blocks(fill_width=True,fill_height=True)as demo:
-    with gr.Blocks() as selection_interface:
+    with gr.Column() as selection_interface:
         with gr.Group():
             gr.Markdown("<h4 style='text-align: center; margin: 0; padding: 5px;'>LLM Settings</h4>")
             with gr.Row():
@@ -20,7 +20,7 @@ with gr.Blocks(fill_width=True,fill_height=True)as demo:
                 api_value = gr.Textbox(label=f"Enter auth key", interactive=True,value="HF_TOKEN")
             with gr.Row():
                 llm_name= gr.Textbox(label="Model name", interactive=True,value="meta-llama/Llama-3.3-70B-Instruct")
-        
+            
 
         with gr.Group():
             gr.Markdown("<h4 style='text-align: center; margin: 0; padding: 5px;'>Image model Settings</h4>")
@@ -33,9 +33,12 @@ with gr.Blocks(fill_width=True,fill_height=True)as demo:
                 image_style = gr.Textbox(label="Image style",interactive=True)
         api_key_button = gr.Button("Connect")
 
-
         api_auth_dropdown.change(fn=update_placeholders, inputs=[api_selection,api_auth_dropdown,gr.State(default_keys)],outputs=api_value)
         api_selection.change(fn=update_placeholders, inputs=[api_selection,api_auth_dropdown,gr.State(default_keys)],outputs=api_value)
+        
+
+
+        
     with gr.Row(visible=False) as main_interface:
         with gr.Column():
             chat_story = gr.ChatInterface(
@@ -51,8 +54,8 @@ with gr.Blocks(fill_width=True,fill_height=True)as demo:
             image= gr.Image(label="Image",height=600,)
             image_button = gr.Button("Generate Image")
     image_button.click(fn=GenerateImage,inputs=[chat_story.chatbot,image_style],outputs=image,api_name="generateImage")
-    api_key_button.click(fn=add_key_and_show_interface,inputs=[api_selection,api_auth_dropdown,api_value,llm_name,image_model_name],outputs=[main_interface,selection_interface])
     change_api.click(fn=add_key_and_show_interface,inputs=[api_selection,api_auth_dropdown,api_value,llm_name,image_model_name],outputs=[selection_interface,main_interface])
+    api_key_button.click(fn=add_key_and_show_interface,inputs=[api_selection,api_auth_dropdown,api_value,llm_name,image_model_name],outputs=[main_interface,selection_interface])
     chat_story.chatbot.change(
         fn=conditional_generate_image,
         inputs=[chat_story.chatbot, chat_story.additional_inputs[2],image_style],
