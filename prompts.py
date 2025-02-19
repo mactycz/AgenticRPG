c178a05ad31628bbca4c1e12fd762da4e9fce9b7
mactycz <mactycz@gmail.com>
2025-02-19 23:00:43 +0100

    Added prompts for true RPG

--- prompts.py
+++ prompts.py
@@ -3,3 +3,14 @@ summarize_for_image= "You are helpful and creative summarizer. You summarize sto
 abcd_options = "Generate 4 options for the player to choose from. Each option should be a possible action or a choice that the player can make. Make them different and interesting. Each option should be up to 50 words long. "
 initialize_story="Welcome to the RPG adventure! Describe the theme of the story, and I'll be a game master. "
 summarize_for_future = "You are helpful and creative summarizer. You summarize the session, and it should include all of the important events and characters, and their characteristics. Make it as long as necessary, to include everything important. This summary should be useful for future sessions for continuation of the story."
+true_rpg_roll_need = """Based on the following interaction, decide if the player needs to roll on a stat or not in a classic RPG DND style.
+Your answer should be in parenthesis between two || signs. Arguments should be delimeted with comma. First argument should be True if roll is needed, False if not. 
+Second argument should be stat.
+Third argument should be difficulty - between 1 and 30. 1-5 is incredibely easy, 6-10 is easy, 10-15 medium, 15-20 hard, over 20 is very hard, with 30 almost impossible. 10 should be dafault.
+Example results:
+||(True,Charisma,15)||
+||(False)||
+"""
+true_rpg_output = """
+Based on the classic RPG DND stat test result, continue the story and what happens next.
+"""
