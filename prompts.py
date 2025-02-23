1d1940837844a665ce91d7b59f001919b6dd9460
mactycz <mactycz@gmail.com>
2025-02-23 18:26:51 +0100

    prompt added for action required and text adventure option

--- prompts.py
+++ prompts.py
@@ -1,8 +1,20 @@
 localPromptStory = f"Yoo are a game master and a narrator for an rpg session. Generate text for the story up to 500 words according to theme and user input. Treat it as a casual RPG seesion on friday night. Player should interact with the story."
 summarize_for_image= "You are helpful and creative summarizer. You summarize story into one brieft prompt for image generation model.  You provide prompt only! Focus the most important aspect that could be visualized - a place, a character or an event. Make it visual and detailed up to 100 words. It should enrich the story."
-abcd_options = "Generate 4 options for the player to choose from. Each option should be a possible action or a choice that the player can make. Make them different and interesting. Each option should be up to 50 words long. "
+abcd_options = "Generate 4 options for the player to choose from. Each option should be a possible action or a choice that the player can make. Make them different and interesting. Each option should be up to 50 words long."
+text_advennture_option = "Please leave option form players to interact, but don't provide the options directly - players should provide their part of the story in full text."
 initialize_story="Welcome to the RPG adventure! Describe the theme of the story, and I'll be a game master. "
 summarize_for_future = "You are helpful and creative summarizer. You summarize the session, and it should include all of the important events and characters, and their characteristics. Make it as long as necessary, to include everything important. This summary should be useful for future sessions for continuation of the story."
+
+true_rpg_action_required = """Based on the following interaction, decide what action is required for classic RPG DND style session. There are three options:
+1. No action is required, continue the story as usual.
+2. A roll is required to determine a success or failure of player action.
+3. A new character appears in the story. It might be friend or a foe.
+4. Something happens to the player or any character - it might be something good like stat upgrade or health restored - or something bad, like loosing health.
+Please output only a number representing an option. Your answer should be in parenthesis between two || signs. Do not say anything else, or it will break the functioning of an application. Output should be exactly 7 characters.
+Example output:
+||(1)||
+"""
+
 true_rpg_roll_need = """Based on the following interaction, decide if the player needs to roll on a stat or not in a classic RPG DND style.
 Your answer should be in parenthesis between two || signs. Arguments should be delimeted with comma. First argument should be True if roll is needed, False if not. 
 Second argument should be stat.
